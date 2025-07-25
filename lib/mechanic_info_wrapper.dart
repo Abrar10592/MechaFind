5e60323f10f5427172aa9b0e85125c71f3d8bf71
Abrar10592 <abraribtesham@gmail.com>
2025-07-25 16:21:10 +0600

    activity of mechanic

---
+++ lib/mechanic_info_wrapper.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'detailed_mechanic_card.dart';
+
+class MechanicInfoWrapper extends StatelessWidget {
+  final Map<String, dynamic> mechanicRaw;
+
+  const MechanicInfoWrapper({Key? key, required this.mechanicRaw})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final adaptedMechanic = <String, dynamic>{
+      'name': mechanicRaw['name'] ?? 'Unknown',
+      'address': mechanicRaw['address'] ?? '',
+      'online': mechanicRaw['online'] ??
+          (mechanicRaw['status']?.toString().toLowerCase() == 'online'),
+      'distance': mechanicRaw['distance'] ?? '',
+      'rating': mechanicRaw['rating'] ?? 0.0,
+      'reviews': mechanicRaw['reviews'] ?? 0,
+      'response': mechanicRaw['response'] ?? mechanicRaw['responseTime'] ?? '',
+      'photoUrl': mechanicRaw['photoUrl'] ?? '',
+      'services': mechanicRaw['services'] ?? <String>[],
+    };
+    return DetailedMechanicCard(mechanic: adaptedMechanic);
+  }
+}
